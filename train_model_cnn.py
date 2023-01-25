9e1e9fc52233f7f3338f492e678377415cba0be0
Jalal Abboud <jalal.abboud@annalect.com>
2023-01-25 12:46:15 +0200

    removed repeat from cnn

--- train_model_cnn.py
+++ train_model_cnn.py
@@ -127,8 +127,8 @@ callbacks_list = [
 start = datetime.datetime.now()
 # training
 history = model.fit(
-    train_data.repeat(2),
-    validation_data=val_data.repeat(2),
+    train_data,
+    validation_data=val_data,
     epochs=epochs,
     callbacks=callbacks_list,
     # steps_per_epoch=steps_per_epoch,
